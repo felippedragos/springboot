f12441f0790ce8a66f5c1819753ff05b75c83961
Felippe Dragos <felippe.dragos@hotmail.com>
2026-01-19 11:15:26 -0300

    Projeto de estudo da arquitetura do Spring

---
+++ ArquiteturaSpring/src/test/java/br/com/dragos/ArquiteturaSpring/ArquiteturaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dragos.ArquiteturaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
